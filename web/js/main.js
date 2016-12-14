dfd16a236f3fcc5e5857c2e8b0fdac0c087830b0
Florian Breuer <f.breuer@a3plus.de>
2016-12-14 17:26:45 +0100

    fix jquery bug

--- web/js/main.js
+++ web/js/main.js
@@ -6,7 +6,7 @@ function loadChatRoom() {
     $(".chat-start-container").fadeOut(350);
     $(".loading-container").fadeIn(700);
     setTimeout(function(){
-        window.location.href = "http://www.chat.dev/chatroom/1234";
+        window.location.href = "/chatroom/1234";
     }, 3500);
 }
 
